f19124eadba6987350de9c37ad20d13a221d48f4
Katja Sabando <katja.sabando@gmail.com>
2023-02-27 12:02:45 +0100

    Build page structure

---
+++ app/page.js
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: 'Ka-sama welcome page',
+  description:
+    'Discover the unique blend of Pilates and island culture with our online store. Our certified instructors bring the energy and spirit of the Philippines to each session. Shop now for personalized classes and equipment to help you reach your fitness goals.',
+  icons: {
+    shortcut: '/icon.svg',
+  },
+};
+
+export default function HomePage() {
+  return (
+    <main>
+      <div>Here comes some content later on.</div>
+    </main>
+  );
+}
