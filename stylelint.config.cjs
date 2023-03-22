1f90083077590833baa9c9f7568f6ce606a0aef6
Katja Sabando <katja.sabando@gmail.com>
2023-03-22 11:18:37 +0100

    Add dependecies

--- stylelint.config.cjs
+++ stylelint.config.cjs
@@ -1,6 +1,10 @@
 /** @type { import('stylelint').Config } */
 const config = {
-  extends: ['stylelint-config-recommended'],
+  extends: [
+    'stylelint-config-recommended',
+    'stylelint-config-recommended-scss',
+    'stylelint-config-css-modules',
+  ],
   rules: {
     'no-descending-specificity': null,
     // Allow files without any styles
