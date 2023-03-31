55369146e154c5216c27fd0fb74859a6542d4a25
Katja Sabando <katja.sabando@gmail.com>
2023-03-31 15:24:16 +0200

    Update playwright test

--- playwright/register.spec.ts
+++ playwright/register.spec.ts
@@ -1,16 +1,16 @@
 import { test } from '@playwright/test';
 
 test('test', async ({ page }) => {
-  await page.goto('http://localhost:3002/');
+  await page.goto('http://localhost:3000/');
   await page.getByRole('button').click();
   await page.getByRole('link', { name: 'EVENTS' }).click();
   await page.getByRole('heading', { name: 'Tagalog for beginners' }).click();
-  await page.getByText('07.05.2023 at 18:00').click();
+  await page.getByText('07/05/2023 at 18:00').click();
   await page.locator('.card-actions > .btn').first().click();
   await page.getByLabel('Username').click();
-  await page.getByLabel('Username').fill('lily');
+  await page.getByLabel('Username').fill('simon');
   await page.getByLabel('Username').press('Tab');
   await page.getByLabel('Password').click();
-  await page.getByLabel('Password').fill('123');
+  await page.getByLabel('Password').fill('123456');
   await page.getByRole('button', { name: 'Login' }).click();
 });
