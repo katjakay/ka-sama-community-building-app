a78fe89bb3aa3ec297a67d9d86954745873c949a
Katja Sabando <katja.sabando@gmail.com>
2023-03-31 16:01:33 +0200

    Update playwright test

--- playwright/register.spec.ts
+++ playwright/register.spec.ts
@@ -1,16 +1,27 @@
-import { test } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 
-test('test', async ({ page }) => {
-  await page.goto('http://localhost:3000/');
-  await page.getByRole('button').click();
-  await page.getByRole('link', { name: 'EVENTS' }).click();
-  await page.getByRole('heading', { name: 'Tagalog for beginners' }).click();
-  await page.getByText('07/05/2023 at 18:00').click();
-  await page.locator('.card-actions > .btn').first().click();
-  await page.getByLabel('Username').click();
-  await page.getByLabel('Username').fill('simon');
-  await page.getByLabel('Username').press('Tab');
-  await page.getByLabel('Password').click();
-  await page.getByLabel('Password').fill('123456');
-  await page.getByRole('button', { name: 'Login' }).click();
+test('login and register test', async ({ page }) => {
+  await page.goto('http://localhost:3000/login');
+
+  await expect(
+    page.getByRole('heading', { name: 'Welcome back,' }),
+  ).toBeVisible();
+
+  await expect(page.getByRole('link', { name: 'Register now' })).toBeVisible();
+
+  await page.getByRole('link', { name: 'Register here' }).click();
+
+  await expect(page).toHaveURL('http://localhost:3000/register');
+
+  await expect(
+    page.getByRole('heading', { name: 'Join KA-SAMA!' }),
+  ).toBeVisible();
+
+  await expect(
+    page.getByRole('link', { name: 'Already a user? Login here' }),
+  ).toBeVisible();
+
+  await page.getByRole('link', { name: 'Login' }).click();
+
+  await expect(page).toHaveURL('http://localhost:3000/login');
 });
